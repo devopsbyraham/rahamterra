61899daaab677eb2bc5f0af7a7208b2c01b7f4e3
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2023-10-31 08:08:56 +0530

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-sudo -i
-yum update -y
-yum install httpd -y
+yum install httpd git -y
 systemctl start httpd
-
+systemctl status httpd
+cd /var/www/html
+git clone https://github.com/CleverProgrammers/pwj-netflix-clone.git
+mv pwj-netflix-clone/* .
