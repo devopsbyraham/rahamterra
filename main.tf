d1cdf896e634725fb94e1585f29f4eb3b5cced6a
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2023-10-31 08:16:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,20 +38,11 @@ resource "aws_subnet" "web-subnet-2" {
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
  tags = {
-    Name = "Web-1a"
+    Name = "Web-2a"
   }
 }
 
-resource "aws_subnet" "web-subnet-2" {
-  vpc_id                  = aws_vpc.my-vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
 
-  tags = {
-    Name = "Web-2b"
-  }
-}
 
 # Create Application Public Subnet
 resource "aws_subnet" "application-subnet-1" {
